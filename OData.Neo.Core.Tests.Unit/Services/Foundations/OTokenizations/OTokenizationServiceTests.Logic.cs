eeeb737f3e2f5e2bf62c75fa533e6b070be1b77d
Callum Marshall <callummarshall9@googlemail.com>
2024-02-16 15:58:17 +0000

    ShouldOTokenizeSelectExpand -> FAIL

--- OData.Neo.Core.Tests.Unit/Services/Foundations/OTokenizations/OTokenizationServiceTests.Logic.cs
+++ OData.Neo.Core.Tests.Unit/Services/Foundations/OTokenizations/OTokenizationServiceTests.Logic.cs
@@ -135,6 +135,107 @@ namespace OData.Neo.Core.Tests.Unit.Services.Foundations.OTokenizations
         }
 
         [Fact]
+        public void ShouldOTokenizeSelectExpand()
+        {
+            // given
+            var unidentifiedOTokens = new OToken[]
+            {
+                new OToken
+                {
+                    ProjectedType = ProjectedTokenType.Keyword,
+                    RawValue = "$expand",
+                },
+                new OToken
+                {
+                    ProjectedType = ProjectedTokenType.Equals,
+                    RawValue = "=",
+                },
+                new OToken
+                {
+                    ProjectedType = ProjectedTokenType.Property,
+                    RawValue = "LibraryCards",
+                },
+                new OToken
+                {
+                    ProjectedType = ProjectedTokenType.Brackets,
+                    RawValue = "(",
+                },
+                new OToken
+                {
+                    ProjectedType = ProjectedTokenType.Keyword,
+                    RawValue = "$select",
+                },
+                new OToken
+                {
+                    ProjectedType = ProjectedTokenType.Equals,
+                    RawValue = "=",
+                },
+                new OToken
+                {
+                    ProjectedType = ProjectedTokenType.Property,
+                    RawValue = "Name",
+                },
+                new OToken
+                {
+                    ProjectedType = ProjectedTokenType.Brackets,
+                    RawValue = ")",
+                },
+            };
+
+            OToken[] inputTokens = unidentifiedOTokens;
+
+            OToken expectedToken = new OToken
+            {
+                Type = OTokenType.Root,
+
+                Children = new List<OToken>
+                {
+                    new OToken
+                    {
+                        RawValue = "$expand",
+                        Type = OTokenType.Expand,
+                        ProjectedType = ProjectedTokenType.Keyword,
+
+                        Children = new List<OToken>
+                        {
+                            new OToken
+                            {
+                                ProjectedType = ProjectedTokenType.Property,
+                                RawValue = "LibaryCards",
+                                Type = OTokenType.Property,
+                                Children = new List<OToken>
+                                {
+                                    new OToken
+                                    {
+                                        RawValue = "$select",
+                                        Type = OTokenType.Select,
+                                        ProjectedType = ProjectedTokenType.Keyword,
+                                        Children = new List<OToken>
+                                        {
+                                            new OToken
+                                            {
+                                                RawValue = "Name",
+                                                Type = OTokenType.Property,
+                                                ProjectedType = ProjectedTokenType.Property,
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            // when
+            OToken actualToken =
+                this.tokenizationService.OTokenize(inputTokens);
+
+            // then
+            actualToken.Should().BeEquivalentTo(expectedToken);
+        }
+
+        [Fact]
         public void ShouldOTokenizeEmptyArrays()
         {
             // given
